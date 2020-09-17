c03f051b9ab51fa42078d4679f8fa9c23c3c83f4
Vivek Singh Bhandari <ervivekbhandari@gmail.com>
2020-09-17 13:29:14 +0530

    4 Levels created. Die and Transcending SFX added.

--- Assets/Scripts/Jet.cs
+++ Assets/Scripts/Jet.cs
@@ -1,27 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Jet : MonoBehaviour
 {
 
     Rigidbody rigidbody;
     AudioSource audioSource;
+
     [SerializeField] float THRUST_FORCE = 900f;
     [SerializeField] float ROTATE_SPEED = 200f;
+
+    int currentScene;
+    [SerializeField] int lastLevelIndex = 3;
+
+    float TIME_DELAY = 2f;
+
+    [SerializeField] AudioClip[] audioClip; 
+
+
+    enum State { Alive, Die, Transcending}
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        currentScene = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        switch (state)
+        {
+            case State.Alive:
+                Thrust();
+                Rotate();
+                break;
+
+            case State.Transcending:
+                audioSource.PlayOneShot(audioClip[1]);
+                break;
+
+            case State.Die:
+                audioSource.PlayOneShot(audioClip[2]);
+                break;
+
+            default:
+                audioSource.Stop();
+                break;
+        }
     }
 
     private void Thrust()
@@ -31,7 +63,7 @@ public class Jet : MonoBehaviour
             rigidbody.AddRelativeForce(Vector3.up * THRUST_FORCE * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(audioClip[0]);
             }
 
         }
@@ -57,5 +89,38 @@ public class Jet : MonoBehaviour
             rigidbody.AddRelativeForce(Vector3.zero);
         }
         rigidbody.freezeRotation = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Wall":
+                state = State.Die;
+                Invoke("RestartScene", TIME_DELAY);
+                break;
+
+            case "Finish":
+                state = State.Transcending;
+                Invoke("CheckForNextLevel", TIME_DELAY);
+                break;
+        }
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void CheckForNextLevel()
+    {
+        if (currentScene == lastLevelIndex)
+        {
+            SceneManager.LoadScene(0);
+            return;
+        }
+        SceneManager.LoadScene(currentScene + 1);
     }
 }
